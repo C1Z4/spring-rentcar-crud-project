89cd5c89909b9e0f852a37886b3c3a6830508d54
Jiwon Ryu <88939170+lalabong@users.noreply.github.com>
2025-06-12 15:17:12 +0900

    Refactor: remove layout breadcrumb

--- src/main/java/com/c1z4/rentcar/config/GlobalControllerAdvice.java
+++ src/main/java/com/c1z4/rentcar/config/GlobalControllerAdvice.java
@@ -13,10 +13,11 @@ public class GlobalControllerAdvice {
     private static final Map<String, PageInfo> PAGE_INFO_MAP = new HashMap<>();
 
     static {
-        PAGE_INFO_MAP.put("/member", new PageInfo("회원 관리", "회원 관리"));
-        PAGE_INFO_MAP.put("/car", new PageInfo("차량 관리", "차량 관리"));
-        PAGE_INFO_MAP.put("/rent", new PageInfo("대여 이력 관리", "대여 이력 관리"));
-        PAGE_INFO_MAP.put("/like", new PageInfo("회원별 좋아요", "회원별 좋아요"));
+        PAGE_INFO_MAP.put("/member", new PageInfo("회원 관리"));
+        PAGE_INFO_MAP.put("/member/all-member-list", new PageInfo("전체 회원 조회"));
+        PAGE_INFO_MAP.put("/car", new PageInfo("차량 관리"));
+        PAGE_INFO_MAP.put("/rent", new PageInfo("대여 이력 관리"));
+        PAGE_INFO_MAP.put("/like", new PageInfo("회원별 좋아요"));
     }
 
     @ModelAttribute("pageTitle")
@@ -26,28 +27,15 @@ public class GlobalControllerAdvice {
         return pageInfo != null ? pageInfo.getTitle() : "차량 대여 시스템";
     }
 
-    @ModelAttribute("breadcrumb")
-    public String addBreadcrumb(HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        PageInfo pageInfo = PAGE_INFO_MAP.get(requestURI);
-        return pageInfo != null ? pageInfo.getBreadcrumb() : null;
-    }
-
     private static class PageInfo {
         private final String title;
-        private final String breadcrumb;
 
-        public PageInfo(String title, String breadcrumb) {
+        public PageInfo(String title) {
             this.title = title;
-            this.breadcrumb = breadcrumb;
         }
 
         public String getTitle() {
             return title;
-        }
-
-        public String getBreadcrumb() {
-            return breadcrumb;
         }
     }
 }
